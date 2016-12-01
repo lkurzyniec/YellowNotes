3acd13510e91aaea4daac506227d69f9e91c3981
Lukasz Kurzyniec <lkurzyniec@pgs-soft.com>
2016-12-02 00:16:31 +0100

    added Utils (AppConfiguration and HashProvider)

---
+++ YellowNotes/YellowNotes.Api/Utils/AppConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace YellowNotes.Api.Utils
+{
+    public class AppConfiguration
+    {
+        public static int AccessTokenExpireTimeInMin = GetInt("AccessTokenExpireTimeInMin", 5);
+
+        public static int RefreshTokenExpireTimeInMin = GetInt("RefreshTokenExpireTimeInMin", 15);
+
+        protected static string GetString(string key, string defaultValue)
+        {
+            var value = ConfigurationManager.AppSettings[key];
+            return value ?? defaultValue;
+        }
+
+        protected static int GetInt(string key, int defaultValue)
+        {
+            return int.Parse(GetString(key, defaultValue.ToString()));
+        }
+    }
+}
---
+++ YellowNotes/YellowNotes.Api/Utils/HashProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace YellowNotes.Api.Utils
+{
+    public class HashProvider
+    {
+        public static string Get(string input)
+        {
+            HashAlgorithm hashAlgorithm = new SHA256CryptoServiceProvider();
+            byte[] byteValue = System.Text.Encoding.UTF8.GetBytes(input);
+            byte[] byteHash = hashAlgorithm.ComputeHash(byteValue);
+            return Convert.ToBase64String(byteHash);
+        }
+    }
+}
