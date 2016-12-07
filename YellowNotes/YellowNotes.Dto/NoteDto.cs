d48772f40d6887e5c620f23ffe1249d7ee9505cd
Lukasz Kurzyniec <lkurzyniec@gmail.com>
2016-12-07 01:17:06 +0100

    small code improvements in Help Pages

--- YellowNotes/YellowNotes.Dto/NoteDto.cs
+++ YellowNotes/YellowNotes.Dto/NoteDto.cs
@@ -18,9 +18,15 @@ namespace YellowNotes.Dto
         [Required]
         public string Content { get; set; }
 
+        /// <summary>
+        /// Rank of Note
+        /// </summary>
         [Range(0, 5)]
         public int Rank { get; set; }
 
+        /// <summary>
+        /// Device type where Note were created
+        /// </summary>
         public string CreatedAt { get; set; }
     }
 }
